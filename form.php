f0464afd15afcfd58e118af51c95485a41740cd7
uphi13 <144438325+uphi13@users.noreply.github.com>
2023-09-26 15:50:56 +0700

    Update form.php

--- form.php
+++ form.php
@@ -5,35 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulir Pembelian</title>
     <style>
-		fieldset{
+        fieldset{
 			width: 250px;
 			border-color: black;
 			box-shadow: 2px 2px 4px #333;
 		}
-</style>
+    </style>
 </head>
 <body>
     <fieldset>
-        <legend align="center">Form Pembelian</legend>
-        <pre>------------------------------------------</pre>
-    <h4>******Toko Cat Guna Bangun Jaya******</h4>
-        <pre>------------------------------------------</pre>
-    <form action="hitung.php" method="POST">
-        <pre>Nama Customer : <input type="text" name="nama"></pre>
-        <pre>Alamat        : <input type="text" name="alamat"></pre>
-        <pre>Jenis Cat     : <select name="jenis" id="">
-            <option value="-">-</option>
-            <option value="Bituminous Paint">Bituminous Paint</option>
-            <option value="Chlorinated Rubber">Chlorinated Rubber</option>
-            <option value="Vinyl">Vinyl</option>
-        </select></pre>
-        <pre>Warna Cat     : <input type="radio" name="warna" id="" value="Merah">Merah <input type="radio" name="warna" id="" value="Biru">Biru <input type="radio" name="warna" id="" value="Kuning">Kuning</pre>
-        <pre>Jumlah Beli   : <input type="text" name="jumlah_beli"></pre>
-        <pre>------------------------------------------</pre>
-        <input type="submit" name="btn_hitung" value="Hitung">
-        <input type="reset" name="btn_batal" value="Batal">
-        <pre>------------------------------------------</pre>
-    </form>
+        <legend>Formulir</legend>
+        <pre>=======================================</pre>
+        <h3>--------Toko Cat Jaya Berkah-------</h3>
+        <pre>=======================================</pre>
+        <form action="hitung.php" method="POST">
+            <pre>Nama        : <input type="text" name="nama"></pre>
+            <pre>Alamat      : <input type="text" name="alamat"></pre>
+            <pre>Jenis Cat   : <select name="jenis">
+                <option value="-">-</option>
+                <option value="Bituminous Paint">Bituminous Paint</option>
+                <option value="Chlorinated Rubber">Chlorinated Rubber</option>
+                <option value="Vinyl">Vinyl</option>
+            </select></pre>
+            <pre>Warna Cat   : <input type="radio" name="warna" value="Biru">Biru <input type="radio" name="warna" value="Merah">Merah <input type="radio" name="warna" value="Hijau">Hijau</pre>
+            <pre>Jumlah Beli : <input type="text" name="jumlah"></pre>
+        <pre>=======================================</pre>
+        <input type="submit" name="hitung" value="Hitung">
+        <input type="reset" name="batal" value="Batal">
+        </form>
     </fieldset>
 </body>
 </html>
